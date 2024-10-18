76e18d4046e98f2cbca8e2ccf86472efe5197f8f
Miguel Ángel Monzón Martínez <miguelmonzon.inf@gmail.com>
2024-10-17 22:04:32 -0300

    "Added CSS styles to ControlPresupuesto.vue component"

--- src/components/ControlPresupuesto.vue
+++ src/components/ControlPresupuesto.vue
@@ -25,4 +25,64 @@ import imagen from "../assets/img/grafico.jpg"
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.dos-columnas {
+    display: flex;
+    flex-direction: column;
+}
+
+.dos-columnas> :first-child {
+    margin-bottom: 3rem;
+}
+
+@media (min-width: 768px) {
+    .dos-columnas {
+        flex-direction: row;
+        gap: 4rem;
+        align-items: center;
+    }
+
+    .dos-columnas> :first-child {
+        margin-bottom: 0;
+    }
+
+    .contenedor-presupuesto p {
+
+        text-align: left;
+    }
+}
+
+.contenedor-presupuesto {
+    width: 100%;
+}
+
+.contenedor-presupuesto p {
+    font-size: 2.4rem;
+    text-align: center;
+    color: var(--gris-oscuro);
+}
+
+.contenedor-presupuesto span {
+    font-weight: 900;
+    color: var(--azul);
+
+}
+
+.reset-app {
+    background-color: #db2777;
+    border: none;
+    padding: 1rem;
+    width: 100%;
+    color: var(--blanco);
+    font-weight: 900;
+    text-transform: uppercase;
+    border-radius: 1rem;
+    transition-property: background-color;
+    transition-duration: 300ms;
+}
+
+.reset-app:hover {
+    cursor: pointer;
+    background-color: #c11d67;
+}
+</style>
